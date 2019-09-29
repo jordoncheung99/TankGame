0f7cd192ceb8c98bc79287afd6c2bd03e460cf66
jordoncheung99 <37314990+jordoncheung99@users.noreply.github.com>
2019-09-28 20:39:10 -0600

    Added Processing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
-public class Main {
+import processing.core.PApplet;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main extends PApplet {
+
+    public static void main(String[] args){
+        PApplet.main("Main");
+    }
+
+    public void settings(){
+        size(300,300);
+    }
+
+    public void setup(){
+        fill(120,50,240);
+    }
+
+    public void draw(){
+        ellipse(width/2,height/2,second(),second());
     }
 }
