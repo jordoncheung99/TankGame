85bb5816c2bc41afc6f03e1d3c959867e0c8a915
Harney Isip <harney_isip@yahoo.com>
2019-11-06 00:35:42 -0700

    login and register functionality in client stored in usernames.txt

--- src/Client.java
+++ src/Client.java
@@ -1,17 +1,13 @@
-/*
- * A simple TCP client that sends messages to a server and display the message
-   from the server.
- * For use in CPSC 441 lectures
- * Instructor: Prof. Mea Wang
- */
 
 //create new isntance for each player
 //change the wording
 //if certain keywords are entered then do specific things like login and register
-//
+//login
+
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 class Client {
 
@@ -33,59 +29,71 @@ class Client {
                 new BufferedReader(new
                         InputStreamReader(clientSocket.getInputStream()));
 
+        //----------Usernames--------------------------------
+        String userName = "";
+        File file = new File("usernames.txt");
+        Scanner s = new Scanner(file);
+        ArrayList<String> userArray = new ArrayList<String>();
+        while (s.hasNext()){
+            userName = s.next();
+            userArray.add(userName);
+        }
+        s.close();
+        //System.out.println(userArray.get(2));
+
+        //-----------------------------------------------
         // Initialize user input stream
+        FileWriter fw = new FileWriter(file,true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        //bw.write("fuk this");
+        //bw.close();
         String line;
         BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader nameInput =
                 new BufferedReader(new InputStreamReader(System.in));
 
         // Get user input and send to the server
         // Display the echo meesage from the server
-        System.out.print("Please enter a message to be sent to the server ('logout' to terminate): ");
+        System.out.print("login/register/exit: ");
         line = inFromUser.readLine();
-        while (!line.equals("logout"))
+        while (!line.equals("exit"))
         {
             // Send to the server
             outBuffer.writeBytes(line + '\n');
-
             //if the input is list then list all the files in current directory
-            if (line.equals("list"))
-            {
-                //get the current directory
-                File currentDir = new File(".");
-                //list of the files in the current directory
-                File[] filesList = currentDir.listFiles();
-                //get all the files in the current directory and print it
-                for(File file : filesList){
-                    if(file.isDirectory())
-                    {
-                        System.out.println(file.getName());
-                    }
-                    if(file.isFile())
-                    {
-                        System.out.println(file.getName());
-                    }
+            if (line.equals("login")){
+                System.out.println("Enter login Username: ");
+                line = nameInput.readLine();
+                boolean isUser = userArray.contains(line);
+                if(isUser){
+                    System.out.println("Welcome:" + line);
+                }
+                else{
+                    System.out.println("Can't find username: " + line);
                 }
             }
+            else if (line.equals("register")){
+                // BufferedWriter bw = new BufferedWriter(fw);
+                System.out.println("Enter Username to register: ");
+                line = nameInput.readLine();
+                System.out.println("New User:" + line);
+                userArray.add(line);
+                bw.write(line + "\n");
+                //bw.close();
 
-	   /*
-	   if (line.equals("get"){
+            }
 
-	   }
-	   */
-
-            //if its not list or get then print unknown command
-            //currently have a bug where it prints the input before
             else{
-                // Getting response from the server
-                line = inBuffer.readLine();
-                System.out.println("Server: Unknown Command: " + line);
+                System.out.println("Only type login or register");
             }
 
 
-            System.out.print("Please enter a message to be sent to the server ('logout' to terminate): ");
+            System.out.print("login/register/exit: ");
             line = inFromUser.readLine();
-        }
 
+        }
+        bw.close();
         // Close the socket
         clientSocket.close();
     }
